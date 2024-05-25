1a3ee5c8a81c7998caa27cac447a08d57a47c965
Vinnie Leung <vleung20@gmail.com>
2024-05-25 15:00:15 +0100

    refined fizzbuzz logic

--- src/components/FizzBuzzer.vue
+++ src/components/FizzBuzzer.vue
@@ -3,42 +3,51 @@ import { ref } from "vue";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
-defineProps<{ msg: string }>();
+// defineProps<{ msg: string }>();
 
-const count = ref(0);
+// Little trick to force a re-render of the component by incrementing the 'key' value
+// TODO: Could change to bind 'key' to the outputString instead...
+const inputNum = ref("");
+const outputHtml = ref("");
 
-const inputNum = ref(null);
-const outputString = ref("FIZZ!");
+const fbCheck = () => {
+  let stringBuilder = "";
 
-const fbCheck = (event) => {
   if (inputNum.value % 3 == 0) {
-    outputString.value = "FIZZ!";
+    stringBuilder += "FIZZ";
   }
   if (inputNum.value % 5 == 0) {
-    outputString.value = "BUZZ!";
+    stringBuilder += "BUZZ";
   }
 
-  if (inputNum.value % 15 == 0) {
-    outputString.value = "FIZZBUZZ!";
+  if (stringBuilder.length > 0) {
+    outputHtml.value = `It's a <span class=\"anim-logo-gradient anim-turbo\">${stringBuilder}!</span>`;
+  } else {
+    outputHtml.value =
+      "It's not a multiple of 3 or 5 🤷 <br/><br/> Try a different number!";
   }
-
-  console.log(inputNum.value);
 };
 </script>
 
 <template>
   <div class="items-center flex flex-wrap gap-3 grow">
     <Input
+      type="text"
+      inputmode="numeric"
+      pattern="[0-9]*"
       v-model="inputNum"
       class="min-w-fit w-auto grow"
       placeholder="e.g. 102"
+      @keyup.enter="fbCheck"
     />
     <Button class="grow md:max-w-fit" @click="fbCheck">Check!</Button>
   </div>
-  <div id="answer" class="output text-center text-6xl anim-pop-out">
-    It's a
-    <span class="anim-logo-gradient anim-turbo">{{ outputString }}!</span>
-  </div>
+  <div
+    id="answer"
+    class="output text-center text-4xl lg:text-6xl anim-pop-out"
+    v-html="outputHtml"
+    :key="outputHtml"
+  ></div>
 </template>
 
 <style scoped>
